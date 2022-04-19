2af7bc1a4ffe20e7d7d70ccb223839cb37ae5f0f
ZZalmanis <zigfrids.kristians@inbox.lv>
2022-04-19 23:14:23 +0300

    Initial commit

---
+++ src/main/java/io/codelex/myflightplanner/MyFlightPlannerApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.myflightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFlightPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFlightPlannerApplication.class, args);
+    }
+
+}
